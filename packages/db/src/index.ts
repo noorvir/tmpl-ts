4220b79d7d0e80e53bc78750bc3ee6669cf465b6
Noorvir Aulakh <noorvir.aulakh@gmail.com>
2025-12-28 23:20:05 +0100

    Update Prisma

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,11 +1,21 @@
+import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { Pool } from "pg";
 
 import { env } from "@acme/config/env";
 
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient | undefined;
+  pool: Pool | undefined;
 };
-const prisma = globalForPrisma.prisma ?? new PrismaClient();
+
+function createPrismaClient() {
+  const pool = new Pool({ connectionString: env.DATABASE_URL });
+  const adapter = new PrismaPg(pool);
+  return new PrismaClient({ adapter });
+}
+
+const prisma = globalForPrisma.prisma ?? createPrismaClient();
 
 if (env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
